a13f63da31b91856f46d6de8abc6e9184fdb3015
Nicolas Gomes <nicolas-gomes2010@hotmail.fr>
2024-03-06 13:58:50 +0100

    FEATURE: CartePizzeria

--- cartePizzeria.py
+++ cartePizzeria.py
@@ -22,28 +22,3 @@ class CartePizzeria:
         raise CartePizzeriaException(f"La pizza {name} n'existe pas dans la carte.")
     
     
-# Création de quelques pizzas
-pizza_margarita = pizza("Margarita", ["tomato sauce", "mozzarella", "basil"], 10.99)
-pizza_pepperoni = pizza("Pepperoni", ["tomato sauce", "mozzarella", "pepperoni"], 12.99)
-
-# Création de la carte de la pizzeria
-carte_pizzeria = CartePizzeria()
-
-# Ajout de pizzas à la carte
-carte_pizzeria.add_pizza(pizza_margarita)
-carte_pizzeria.add_pizza(pizza_pepperoni)
-
-# Affichage du nombre de pizzas dans la carte
-print("Nombre de pizzas dans la carte:", carte_pizzeria.nb_pizzas())
-
-# Suppression d'une pizza de la carte
-carte_pizzeria.remove_pizza("Margarita")
-
-# Affichage mis à jour du nombre de pizzas dans la carte
-print("Nombre de pizzas dans la carte après suppression:", carte_pizzeria.nb_pizzas())
-
-# Tentative de suppression d'une pizza inexistante (va lever une exception)
-try:
-    carte_pizzeria.remove_pizza("Quattro Formaggi")
-except CartePizzeriaException as e:
-    print(f"Erreur: {e}")
